e65276bec7fa04af39e68ad4da04eab78a98cdfa
Gabriella Jeane de Asis <gjdeasis@gmail.com>
2024-06-15 22:18:44 -0500

    Sample solutions for Exercise 1.5 and 1.6

---
+++ Chapter1/src/exercise/ExerciseOneFive.java
@@ -0,0 +1,15 @@
+/*
+ * Prompt: (Compute expressions) Write a program that displays the results for
+ * 9.5 x 4.5 - 2.5 x 3 in the numerator and
+ * 45.5 - 3.5 in the denominator
+ */
+package exercise;
+
+public class ExerciseOneFive {
+	
+	public static void main(String[] args) {
+	
+		System.out.println((9.5*4.5-2.5*3)/(45.5-3.5));
+	
+	}
+}
---
+++ Chapter1/src/exercise/ExerciseOneSix.java
@@ -0,0 +1,11 @@
+/*
+ * Prompt: (Summation of a series) Write a program that displays the results of
+ * 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9
+ */
+package exercise;
+
+public class ExerciseOneSix {
+	public static void main(String[] args) {
+		System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
+	}
+}
