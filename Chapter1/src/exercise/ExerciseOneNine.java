d60ef2d90e4f2aeefe64ef9fc2f20514214e280b
Gabriella Jeane de Asis <gjdeasis@gmail.com>
2024-06-16 19:42:39 -0500

    Sample solution for exercise 1.9

---
+++ Chapter1/src/exercise/ExerciseOneNine.java
@@ -0,0 +1,16 @@
+/*
+ * Prompt: (Area and perimeter of a rectangle) Write a program that displays the area and perimeter of a rectangle
+ * with a width of 4.5 and height of 7.9 using the following formula:
+ * area = width x height
+ */
+package exercise;
+
+public class ExerciseOneNine {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(4.5*7.9);
+		
+	}
+
+}
