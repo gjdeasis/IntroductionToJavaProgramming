701f0ed757b1ce17c54488811158e7eae3d791b4
Gabriella Jeane de Asis <gjdeasis@gmail.com>
2024-06-16 19:37:23 -0500

    Sample solution for exercise 1.8

---
+++ Chapter1/src/exercise/ExerciseOneEight.java
@@ -0,0 +1,15 @@
+/*
+ * Prompt: (Area and perimeter of a circle) Write a program that displays
+ * the area and perimeter of a circle that has a radius of 5.5 using the following formula:
+ * perimeter = 2 x radius x pi
+ * area = radius x radius x pi
+ */
+
+package exercise;
+
+public class ExerciseOneEight {
+	public static void main (String[] args) {
+		System.out.println(2 * 5.5 * 3.14);
+		System.out.println(5.5 * 5.5 * 3.14);
+	}
+}
