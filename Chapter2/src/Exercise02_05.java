c6a8ed06dfee08ebf27d1a26a73895d2c357b811
Gabriella Jeane de Asis <gabriellajeane.deasis.167@my.csun.edu>
2024-06-23 06:10:46 -0500

    Sample solution for exercise 2.5

---
+++ Chapter2/src/Exercise02_05.java
@@ -0,0 +1,27 @@
+/*
+ * (Financial application: calculate tips) Write a program that reads the subtotal
+ * and the gratuity rate, then computes the gratuity and total. For example, if the
+ * user enters 10 for subtotal and 15% for gratuity rate, the program displays $1.5
+ * as gratuity and $11.5 as total. Here is a sample run:
+ * 
+ * Enter the subtotal and a gratuity rate: 10 15
+ * The gratuity is $1.5 and total is $11.5
+ */
+
+import java.util.Scanner;
+
+public class Exercise02_05 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		double subtotal, rate, gratuity, total;
+		
+		System.out.print("Enter the subtotal and a gratuity rate: ");
+		subtotal = input.nextDouble();
+		rate = input.nextDouble();
+		
+		gratuity = subtotal * (rate / 100);
+		total = subtotal + gratuity;
+		
+		System.out.println("The gratuity is $" + gratuity + " and the total is $" + total);
+	}
+}
